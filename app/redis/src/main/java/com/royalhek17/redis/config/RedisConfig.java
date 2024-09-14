f542141f8013b7ed49d98e849779d85351b64976
anhluong660 <anhluong660@gmail.com>
2024-09-14 15:49:16 +0700

    redisson

--- app/redis/src/main/java/com/royalhek17/redis/config/RedisConfig.java
+++ app/redis/src/main/java/com/royalhek17/redis/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.royalhek17.redis.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -32,5 +35,13 @@ public class RedisConfig {
         template.afterPropertiesSet();
 
         return template;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379");
+        return Redisson.create(config);
     }
 }
--- app/redis/src/main/java/com/royalhek17/redis/controller/RedisController.java
+++ app/redis/src/main/java/com/royalhek17/redis/controller/RedisController.java
@@ -1,8 +1,15 @@
 package com.royalhek17.redis.controller;
 
+import com.royalhek17.redis.entities.Item;
 import com.royalhek17.utils.Response;
 import com.royalhek17.redis.dto.Storage;
 import com.royalhek17.redis.service.RedisService;
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RLock;
+import org.redisson.api.RMap;
+import org.redisson.api.RMapCache;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +19,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
 
     @Autowired
     private RedisService redisService;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     @GetMapping("/send-text")
     public Response<String> sendTextMessage(@RequestParam String message) {
@@ -41,4 +54,66 @@ public class RedisController {
         redisService.addNewStorage(storage);
         return Response.success(storage);
     }
+
+    @GetMapping("/redisson-atomic")
+    public Long testAtomic() {
+        RAtomicLong atomicLong = redissonClient.getAtomicLong("Id");
+        return atomicLong.getAndIncrement();
+    }
+
+    @GetMapping("/redisson-map")
+    public Map<Integer, String> testMap() {
+        RMap<Integer, String> map = redissonClient.getMap("Map");
+        map.put(1, "java");
+        map.put(2, "C#");
+        map.put(3, "Javascript");
+        return map;
+    }
+
+    @GetMapping("/redisson-lock")
+    public String testLock() throws InterruptedException {
+        RLock lock = redissonClient.getLock("Lock");
+
+        String status = "Task ";
+
+        if (lock.tryLock(15, 10, TimeUnit.SECONDS)) {
+            status += "Finish";
+            lock.unlock();
+        } else {
+            status += "Fail";
+        }
+
+        return status;
+    }
+
+    @GetMapping("/redisson-cache")
+    public Map<String, String> testCache() {
+        RMapCache<String, String> map = redissonClient.getMapCache("Cache");
+        map.put("Banana", "Fruit", 30, TimeUnit.SECONDS);
+        map.put("Dog", "Animal", 1, TimeUnit.HOURS);
+        map.put("Chair", "Thing", 2, TimeUnit.DAYS);
+        return map;
+    }
+
+    @GetMapping("/redisson-publish")
+    public String testPubSub() {
+        RTopic topic = redissonClient.getTopic("Topic");
+
+        Item item = new Item();
+        item.setName("Gold");
+        item.setNum(12450000);
+
+        topic.publish(item);
+        return "Ok";
+    }
+
+    @GetMapping("/redisson-subscribe")
+    private void listen() {
+        RTopic topic = redissonClient.getTopic("Topic");
+        topic.addListener(Item.class, ((channel, msg) -> {
+            System.out.println("received message: " + msg);
+        }));
+    }
+
+
 }
